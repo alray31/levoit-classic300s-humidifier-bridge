12372e93dcfedfaddd06032f07c8bb11be7624a6
Alain Raymond <115857857+alray31@users.noreply.github.com>
2026-09-23 20:43:39 -0400

    Add files via upload

--- custom_components/levoit_humidifier_bridge/humidifier.py
+++ custom_components/levoit_humidifier_bridge/humidifier.py
@@ -32,9 +32,9 @@ from __future__ import annotations
 import logging
 from typing import Any
 
-from homeassistant.components.humidifier import HumidifierEntity
-from homeassistant.components.humidifier.const import (
+from homeassistant.components.humidifier import (
     HumidifierDeviceClass,
+    HumidifierEntity,
     HumidifierEntityFeature,
 )
 from homeassistant.config_entries import ConfigEntry
